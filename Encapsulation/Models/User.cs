67d1f4732f48193c27f13e66a016e0fc70a5b431
NahidM8 <nahidbm@code.edu.az>
2024-03-08 14:47:32 +0400

    Add project files.

---
+++ Encapsulation/Models/User.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Encapsulation.Models
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        int _age;
+        public int Age 
+        { 
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                    return;
+                }
+
+            }
+        }
+        string _password = "wrong";
+        public string Password 
+        { 
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) return;
+                if (value.Length < 8) return;
+
+                if(!PasswordCheck(value)) return;
+
+                _password = value;
+
+
+            }
+        }
+        private bool PasswordCheck(string password)
+        {
+            bool isDigit = false;
+            bool isUpper = false;
+            foreach (char item in password)
+            {
+                if (char.IsDigit(item)) isDigit = true;
+                if (char.IsUpper(item)) isUpper = true;
+            }
+            return isDigit&&isUpper;
+        }
+
+        public User(string username,string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Encapsulation.Models;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(Console.ReadLine(), Console.ReadLine());
+
+            Console.WriteLine(user.Password);
+        }
+    }
+}
